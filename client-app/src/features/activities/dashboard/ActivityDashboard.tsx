e046a727f8bc3b14d818ceb4ee4d41bd9e96318e
Amit <7742196872amit@gmail.com>
2020-09-16 13:28:34 +0530

    New Changes

---
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import { Grid,List } from 'semantic-ui-react';
+import { IActivity } from '../../../app/models/activity';
+import ActivityList from './ActivityList';
+import ActivityDetails from '../details/ActivityDetails';
+
+interface IProps {
+    activities: IActivity[];
+    selectActivity: (id: string) => void;
+    selectedActivity: IActivity | null;
+    editMode: boolean;
+    setEditMode: (editMode: boolean) => void;
+    setSelectedActivity: (activity: IActivity | null) => void;
+    createActivity: (activity: IActivity) => void;
+    editActivity: (activity: IActivity) => void;
+    deleteActivity: (id: string) => void;
+  }
+  
+
+  const ActivityDashboard: React.FC<IProps> = ({
+    activities,
+    selectActivity,
+    selectedActivity,
+    editMode,
+    setEditMode,
+    setSelectedActivity,
+    createActivity,
+    editActivity,
+    deleteActivity
+  }) => {
+  return (
+    <Grid>
+      <Grid.Column width={10} >
+      <ActivityList
+          activities={activities}
+          selectActivity={selectActivity}
+          deleteActivity={deleteActivity}
+        />
+      {/* <List>
+          {props.activities.map((activity) => (
+            <List.Item key={activity.id}>{activity.title}</List.Item>
+          ))}
+        </List> */}
+      </Grid.Column>
+
+      <Grid.Column width={6} >
+      {selectedActivity && !editMode && (
+          <ActivityDetails
+            activity={selectedActivity}
+            setEditMode={setEditMode}
+            setSelectedActivity={setSelectedActivity}
+          />
+        )}
+          </Grid.Column>
+    </Grid>
+  );
+};
+
+export default ActivityDashboard;
